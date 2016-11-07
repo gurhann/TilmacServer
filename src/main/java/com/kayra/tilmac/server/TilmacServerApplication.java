201090a49750dc29020fe75f679b7120ea0c41aa
Gürhan Küçük <gurhan.kucuk@yandex.com>
2016-11-07 20:30:20 +0300

    Added boot application file.

---
+++ src/main/java/com/kayra/tilmac/server/TilmacServerApplication.java
@@ -0,0 +1,12 @@
+package com.kayra.tilmac.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilmacServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilmacServerApplication.class, args);
+	}
+}
