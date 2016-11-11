6dd492bcd24e3c08fd028119c53ca772fbd7cfc2
Gürhan Küçük <gurhan.kucuk@yandex.com>
2016-11-12 01:41:11 +0300

    Auto stash for revert of "Added component scan."

--- src/main/java/com/kayra/tilmac/server/TilmacServerApplication.java
+++ src/main/java/com/kayra/tilmac/server/TilmacServerApplication.java
@@ -1,9 +1,13 @@
 package com.kayra.tilmac.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class TilmacServerApplication {
 
 	public static void main(String[] args) {
