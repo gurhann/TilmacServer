3f3b3d9e70ffd789a5f14c335ce7da7b8671e75b
Gürhan Küçük <gurhan.kucuk@yandex.com>
2016-11-29 21:16:16 +0300

    Fix for baseword mapper.

--- src/main/java/com/kayra/tilmac/server/mapper/BaseWordMapper.java
+++ src/main/java/com/kayra/tilmac/server/mapper/BaseWordMapper.java
@@ -4,54 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.kayra.tilmac.server.dto.BaseWordDTO;
-import com.kayra.tilmac.server.model.BaseWord;
+import com.kayra.tilmac.server.model.MeaningWord;
 
 public class BaseWordMapper {
 	private BaseWordMapper() {
 
 	}
 
-	public static BaseWord dtoToModel(BaseWordDTO dto) {
+	public static MeaningWord dtoToModel(BaseWordDTO dto) {
 		if (dto == null) {
 			return null;
 		}
-		BaseWord baseWord = new BaseWord();
+		MeaningWord baseWord = new MeaningWord();
 		baseWord.setId(dto.getId());
 		baseWord.setLang(LanguageMapper.dtoToModel(dto.getLang()));
 		baseWord.setWord(dto.getWord());
 		return baseWord;
 	}
 
-	public static BaseWordDTO modelToDto(BaseWord baseWord) {
-		if (baseWord == null) {
+	public static BaseWordDTO modelToDto(MeaningWord word) {
+		if (word == null) {
 			return null;
 		}
 		BaseWordDTO dto = new BaseWordDTO();
-		dto.setId(baseWord.getId());
-		dto.setLang(LanguageMapper.modelToDto(baseWord.getLang()));
-		dto.setWord(baseWord.getWord());
+		dto.setId(word.getId());
+		dto.setLang(LanguageMapper.modelToDto(word.getLang()));
+		dto.setWord(word.getWord());
 		return dto;
 	}
 
-	public static List<BaseWord> dtoToModelList(List<BaseWordDTO> dtoList) {
+	public static List<MeaningWord> dtoToModelList(List<BaseWordDTO> dtoList) {
 		if (dtoList == null || dtoList.isEmpty()) {
 			return null;
 		}
-		List<BaseWord> baseWordList = new ArrayList<>();
+		List<MeaningWord> baseWordList = new ArrayList<>();
 		for (BaseWordDTO dto : dtoList) {
 			baseWordList.add(dtoToModel(dto));
 		}
 		return baseWordList;
 	}
 
-	public static List<BaseWordDTO> modelToDtoList(List<BaseWord> baseWordList) {
+	public static List<BaseWordDTO> modelToDtoList(List<MeaningWord> baseWordList) {
 		if (baseWordList == null || baseWordList.isEmpty()) {
 			return null;
 		}
 		List<BaseWordDTO> dtoList = new ArrayList<>();
-		for (BaseWord baseWord : baseWordList) {
+		for (MeaningWord baseWord : baseWordList) {
 			dtoList.add(modelToDto(baseWord));
 		}
 		return dtoList;
 	}
+
 }
