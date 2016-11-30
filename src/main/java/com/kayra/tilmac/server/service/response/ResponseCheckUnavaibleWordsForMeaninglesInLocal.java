b2459357b401f8697d68a413a4acf01406d38c4c
Gürhan Küçük <gurhan.kucuk@yandex.com>
2016-11-30 22:41:07 +0300

    add equal and hash code.

--- src/main/java/com/kayra/tilmac/server/service/response/ResponseCheckUnavaibleWordsForMeaninglesInLocal.java
+++ src/main/java/com/kayra/tilmac/server/service/response/ResponseCheckUnavaibleWordsForMeaninglesInLocal.java
@@ -24,4 +24,37 @@ public class ResponseCheckUnavaibleWordsForMeaninglesInLocal {
 	public void setUnavailableWordList(List<BaseWordDTO> unavailableWordList) {
 		this.unavailableWordList = unavailableWordList;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((meaninglessWordList == null) ? 0 : meaninglessWordList.hashCode());
+		result = prime * result + ((unavailableWordList == null) ? 0 : unavailableWordList.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResponseCheckUnavaibleWordsForMeaninglesInLocal other = (ResponseCheckUnavaibleWordsForMeaninglesInLocal) obj;
+		if (meaninglessWordList == null) {
+			if (other.meaninglessWordList != null)
+				return false;
+		} else if (!meaninglessWordList.equals(other.meaninglessWordList))
+			return false;
+		if (unavailableWordList == null) {
+			if (other.unavailableWordList != null)
+				return false;
+		} else if (!unavailableWordList.equals(other.unavailableWordList))
+			return false;
+		return true;
+	}
+	
+	
 }
